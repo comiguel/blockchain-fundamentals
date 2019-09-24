4f706a69099b9f74b97753a43c3a4033a3c558d0
Carlos Hincapié <carmihinfer@gmail.com>
2019-09-24 00:30:21 -0300

    Mining the Genesis Block

--- blockchain.js
+++ blockchain.js
@@ -1,5 +1,15 @@
 const SHA256 = require("crypto-js/sha256");
 
+function calculateHash(block) {
+  return SHA256(
+    `${block.index}
+     ${block.timestamp}
+     ${block.previousHash}
+     ${JSON.stringify(block.data)}
+     ${block.nonce}`
+  ).toString();
+}
+
 class Block {
   constructor(index, timestamp, data, previousHash = '') {
     this.index = index;
@@ -7,23 +17,23 @@ class Block {
     this.data = data;
     this.previousHash = previousHash;
     this.nonce = 0;
-    this.hash = this.calculateHash();
+    this.hash = calculateHash(this);
   }
 
-  calculateHash() {
-    return SHA256(
-      `${this.index}
-       ${this.timestamp}
-       ${this.previousHash}
-       ${JSON.stringify(this.data)}
-       ${this.nonce}`
-    ).toString();
+  mineBlock(difficulty) {
+    const prefix = Array(difficulty + 1).join('0');
+    while (this.hash.substring(0, difficulty) !== prefix) {
+      this.nonce++;
+      this.hash = calculateHash(this);
+    }
+    console.log(`Tomó ${this.nonce} iteraciones`);
   }
 }
 
 class Blockchain {
   constructor() {
     const genesisBlock = new Block(0, new Date().getTime(), 'Genesis Block', null);
+    genesisBlock.mineBlock(5);
     this.chain = [genesisBlock];
   }
 
